af2ff8cf6629de27d00e1184b3996c7add9499a4
nicks204 <59753817+nicks204@users.noreply.github.com>
2020-09-29 19:07:21 +0530

    Update StepDef.java

--- src/test/java/validations/StepDef.java
+++ src/test/java/validations/StepDef.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
